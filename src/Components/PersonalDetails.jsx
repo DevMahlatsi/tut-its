e60607e5a52f864a016eccc2a82dd7cdcd69ad58
DevMahlatsi <j.mahlatsi@outlook.com>
2025-12-12 01:01:13 +0200

    PersonalDetails component created.

---
+++ src/Components/PersonalDetails.jsx
@@ -0,0 +1,16 @@
+export default function(){
+  return(
+    <>
+    <div className="personal-details-card">
+      <div className="icon-container">
+        <img className="icon user-icon" src="./src/assets/icons/user-svgrepo-com.svg" alt="" />
+      </div>
+      <div className="personal-details-lbl">
+        <h1 className="test2">Personal Details</h1>
+      </div>
+      
+    </div>
+      
+    </>
+  )
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
+import PersonalDetails from "../Components/PersonalDetails";
 import StudentCard from "../Components/StudentCard";
 
 export default function(){
   return(
     <>
       
+      <div className="home">
+        <StudentCard/>
+        <PersonalDetails/>
+      </div>
       
-      <StudentCard/>
 
       
     </>
